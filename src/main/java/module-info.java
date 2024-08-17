8a52da494bfb7b909c0458b884b06a5918f70750
jabineus <mollah25@my.yorku.ca>
2024-08-17 16:59:54 -0700

    Factory class and pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
-module com.example {
-    requires javafx.controls;
-    requires javafx.fxml;
+module com.pixelcraft {
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
     requires transitive java.desktop;
 
     opens com.pixelcraft to javafx.fxml;
